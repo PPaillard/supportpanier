9ea901c3bd6bff5ba81ad647317354ebc2277d3d
Pierre Paillard <paillard.pierre@gmail.com>
2021-03-24 15:21:18 +0100

    Ajout des methodes panier ajout,suppression et calcultotal()

--- index.js
+++ index.js
@@ -13,16 +13,45 @@ class Article {
 
 class Panier {
   constructor() {
+    this.total = 0;
     this.articles = [];
   }
+  // Fonction permettant d'ajouter un article au panier
   ajouterArticle(article) {
     this.articles.push(article);
   }
+  // Fonction permettant de supprimer un article du panier
+  supprimerArticle(article) {
+    for (let i = 0; i < this.articles.length; i++) {
+      if (this.articles[i].id === article.id) {
+        this.articles.splice(i, 1);
+      }
+    }
+  }
+  // met à jour le prix total du panier
+  mettreAJourTotal() {
+    this.total = 0;
+    // Pour chaque article de mon panier
+    for (let i = 0; i < this.articles.length; i++) {
+      // je multiplie la quantité de l'article indiqué par son prix
+      this.total += this.articles[i].prix * this.articles[i].quantite;
+    }
+  }
 }
+// je créé mes articles
+const veste = new Article("Veste moumoute", 25, 3);
+const tshirt = new Article("T-Shirt de Superman", 9.9, 1);
+const airMax = new Article("Air Max classic", 130, 2);
 
-const monArticle = new Article("Je suis un super article", 9.9, 95);
-
+// Je créé mon panier et j'y ajoute mes articles
 const monPanier = new Panier();
-monPanier.ajouterArticle(monArticle);
+monPanier.ajouterArticle(veste);
+monPanier.ajouterArticle(tshirt);
+monPanier.ajouterArticle(airMax);
 
-console.log(monPanier);
+// je supprime un article de mon panier
+monPanier.supprimerArticle(veste);
+
+// calcul & affichage du total
+monPanier.mettreAJourTotal();
+console.log(monPanier.total);
